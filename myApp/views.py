1474cb5ade2fe2f4931ef068470c66964314f54d
Silvanus Oketch <97881965+Silvokyda@users.noreply.github.com>
2023-07-21 05:58:10 +0000

    contact

--- myApp/views.py
+++ myApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import JsonResponse
 
 # Create your views here.
 def main(request):
@@ -12,3 +14,25 @@ def projects(request):
 
 def contact(request):
     return render(request, "contact.html")
+
+def contact_form_submission(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        # Perform any validation if needed
+
+        # Send email
+        subject = 'New Contact Form Submission'
+        body = f'Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}'
+        from_email = f'{email}' 
+        to_email = 'silvansowino1@gmail.com'  
+        send_mail(subject, body, from_email, [to_email])
+
+        return JsonResponse({'success': True})
+
+    return JsonResponse({'error': 'Invalid request method.'}, status=400)
+
+    

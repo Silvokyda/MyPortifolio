86b7288ad8a89a4faffe1c0ba8b35332870eace1
Silvanus Oketch <deseve48@gmail.com>
2023-07-22 14:43:17 +0300

    print a section

--- myApp/static/script.js
+++ myApp/static/script.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function printSection() {
+    var originalContents = document.body.innerHTML;
+    var section = document.getElementById('sectionToPrint').innerHTML;
+    document.body.innerHTML = section;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
+
+// Event listener to trigger the printing when the button is clicked
+document.getElementById('printButton').addEventListener('click', printSection);
